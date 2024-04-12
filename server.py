15f4899211d878f3984276dfc29c2111e4858017
grinmma <grinm@yandex.ru>
2024-04-12 19:15:07 +0300

    telega code server.py

--- server.py
+++ server.py
@@ -1,71 +1,23 @@
-import flask
 import logging
-import json
+from telegram.ext import Application, MessageHandler, filters
+from config import TOKEN
 
-session_storage = {}
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
 
-app = flask.Flask(__name__)
-
-logging.basicConfig(level=logging.INFO)
-
-
-@app.route("/post", methods=["POST"])
-def post_handler():
-    req_json = flask.request.json
-    logging.info(f"Request: {req_json!r}")
-    response = {
-        "session": req_json["session"],
-        "version": req_json["version"],
-        "response": {
-            "end_session": False
-        }
-    }
-    handle_dialog(req_json, response)
-    logging.info(f"Response: {response!r}")
-    return json.dumps(response)
+logger = logging.getLogger(__name__)
 
 
-def handle_dialog(req_json, response):
-    global session_storage
-    usr_id = req_json["session"]["user_id"]
-
-    if req_json["session"]["new"]:
-        print("pre", session_storage)
-        session_storage[usr_id] = {"suggests": [
-            "Не хочу.", "Не буду.", "Отстань!"]}
-        print("post", session_storage)
-        response["response"]["text"] = "Привет! Купи слона!"
-        response["response"]["buttons"] = get_suggests(usr_id)
-        return
-    if any(map(lambda word: word in req_json["request"]["command"].lower(), ("ладно",
-                                                                             "куплю",
-                                                                             "хорошо",
-                                                                             "покупаю"))):
-        response["response"]["text"] = "Слона можно найти на Яндекс.Маркете!"
-        response["response"]["end_session"] = True
-        return
-    response["response"]["text"] = f"""Все говорят '{req_json["request"]["original_utterance"]}', а ты купи слона!"""
-    response["response"]["buttons"] = get_suggests(usr_id)
+async def echo(update, context):
+    await update.message.reply_text(f'Я получил сообщение '
+                                    f'{update.message.text}')
 
 
-def get_suggests(usr_id):
-    global session_storage
-    print(session_storage)
-    session = session_storage[usr_id]
-    suggests = [
-        {"title": suggest, "hide": True}
-        for suggest in session["suggests"][:2]
-    ]
-    session["suggests"] = session["suggests"][1:]
-    session_storage[usr_id] = session
-    if len(suggests) == 1:
-        suggests.append({
-            "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон",
-            "hide": True
-        })
-    return suggests
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT, echo))
+    logger.info('Бот работает')
+    app.run_polling()
 
-
-if __name__ == "__main__":
-    app.run()
+main()
